64bcd73a4e8c1660f0219104ddb14fc9d8aa931a
edvillatoro2 <edvillatoro2@gmail.com>
2026-03-04 17:27:44 -0500

    adds logo to app

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,42 +1,43 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    48: "public/fetch.png",
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      48: "public/fetch.png",
     },
-    default_popup: 'src/popup/index.html',
+    default_popup: "src/popup/index.html",
   },
-  content_scripts: [{
-    js: ['src/content/main.ts'],
-    // restrict to YouTube only
-    matches: ['https://www.youtube.com/*'],
-  }],
-    oauth2: {
-    client_id: '316008795201-gkc93bslujdvfndr8urqhnf2n7m0nnsp.apps.googleusercontent.com',
-    scopes: [
-      'https://www.googleapis.com/auth/youtube.readonly'
-    ]
+  content_scripts: [
+    {
+      js: ["src/content/main.ts"],
+      // restrict to YouTube only
+      matches: ["https://www.youtube.com/*"],
     },
-    background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module',
+  ],
+  oauth2: {
+    client_id:
+      "316008795201-gkc93bslujdvfndr8urqhnf2n7m0nnsp.apps.googleusercontent.com",
+    scopes: ["https://www.googleapis.com/auth/youtube.readonly"],
   },
-  permissions: ['identity', 'storage', 'tabs', 'sidePanel'],
+  background: {
+    service_worker: "src/background/index.ts",
+    type: "module",
+  },
+  permissions: ["identity", "storage", "tabs", "sidePanel"],
   host_permissions: [
-    'https://www.googleapis.com/*',
-    'https://www.youtube.com/*',
-    'https://accounts.google.com/*',
-    'https://oauth2.googleapis.com/*'
+    "https://www.googleapis.com/*",
+    "https://www.youtube.com/*",
+    "https://accounts.google.com/*",
+    "https://oauth2.googleapis.com/*",
   ],
   side_panel: {
-    default_path: 'src/sidepanel/index.html',
+    default_path: "src/sidepanel/index.html",
   },
-})
+});
